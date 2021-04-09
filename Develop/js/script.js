de90312c51f4ebb8fde383dc7227a1aa20e1ee68
Sharni Zaugg <sharni.greener@gmail.com>
2021-04-08 20:36:19 -0600

    add html and js pseudocode

---
+++ Develop/js/script.js
@@ -0,0 +1,10 @@
+//make date at top adjust each day to show current date
+
+//change span to input on click to allow text 
+//on blur change back to span
+
+//if past: grey. if present: red. if future: green.
+
+//if button clicked, add to local storage. Retrieve from local storage and 
+//display in specified time
+
